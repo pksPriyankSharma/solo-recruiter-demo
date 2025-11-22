f592c5661fb7dc04bda322805f7cfed777571ce4
pksPriyankSharma <priyank74@gmail.com>
2025-11-22 08:19:53 +0530

    Fix: Configure Vercel to build from client folder

--- shared/const.ts
+++
@@ -1,5 +0,0 @@
-export const COOKIE_NAME = "app_session_id";
-export const ONE_YEAR_MS = 1000 * 60 * 60 * 24 * 365;
-
-export const APP_TITLE = "Solo Recruiter";
-export const APP_LOGO = "/logo.svg";
